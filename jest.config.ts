bf5e763f73fc0eddfa15c76e8ac3bef450f3f3b7
li00-lab <liwenhan82@gmail.com>
2025-02-10 21:18:41 -0500

    First commit

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
+
+export default config;
